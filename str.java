5875408371ec736df17f287649a8d018615d2dec
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-08-16 19:22:50 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class str
+{
+public static void main(String a[])
+{
+Scanner s=new Scanner(System.in);
+StringBuffer s1=new StringBuffer(s.next());
+System.out.println(s1.reverse());
+}
+}
