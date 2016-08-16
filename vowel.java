7e66b5f2a1b20ee8d9347772684502980a270974
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-08-16 18:32:30 +0530

    Update vowel.java

--- vowel.java
+++ vowel.java
@@ -3,7 +3,7 @@ class vowel
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.out.println);
+Scanner s=new Scanner(System.in);
 char t=(s.next()).charAt(0);
 if(t=='a'||t=='e'||t=='i'||t=='o'||t=='u'||t=='A'||t=='E'||t=='I'||t=='O'||t=='U')
 System.out.print("vowel");
