8f51c313086a465e6e2138545ed0111da296a190
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-08-16 18:36:31 +0530

    Create alp.java

---
+++ alp.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class alp
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char t=(s.next).charAt(0);
+if(Character.isAlphabetic(t))
+System.out.println("alphabet");
+else
+System.out.println("not alphabet");
+}
+}
