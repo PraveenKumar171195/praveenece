18fb715c82f47cb405713ba4474bc3526d0055b3
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 12:18:31 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -3,10 +3,11 @@ class prime
 {
 pulic static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 int n=s.nextInt(),i,k=0;
-int p[]=new int[];
-for(i=1;i<=n;i++)
+int p[]=new int[n];
+TreeSet ts=new TreeSet();
+for(i=1;i<20;i++)
 {
 if(i%2==0||i%3==0||i%5==0||i%7==0||i%9==0)
 continue;
@@ -20,13 +21,13 @@ for(int j=0;j<p.length;j++)
 {
  for(int t=0;t<p.length;t++)
  {
- if(if(p[j]+p[t]==n)
-  System.out.print(p[j]+" "+p[t]);
-  }
-}
+ if(p[j]+p[t]==n)
+ {
+  ts.add(p[j]);
+  ts.add(p[t]);
+ }
  }
 }
- 
- 
+System.out.print(ts);
 }
 }
