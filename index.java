122cc73ae7643f59e2cd447280610d23e70d5599
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 11:11:26 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class index
+{
+public static void main(String args[])
+{
+int a[]=new int[]{1,3,5,-1,-3},i;
+Arrays.sort(a);
+for(i=0;i<a.length;i++)
+ if(i==a[i])
+  System.out.println(a[i]);
+}
+}
