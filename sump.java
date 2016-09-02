59bd71cb4bcb58604b29cd511cda5ce1e9dd8146
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 18:32:32 +0530

    Update sump.java

--- sump.java
+++ sump.java
@@ -3,24 +3,22 @@ class sump
 {
 public static void main(String args[])
 {
-Scanner s=new Scanner(System.in);
-int n=s.nextint();
+		Scanner s=new Scanner(System.in);
+int n=s.nextInt();
 int y[]=new int[n],sum=0;
+String k=n+"";
 if(n>9)
 {
-for(int i=0;i<y.length;i++)
+for(int i=0;i<k.length();i++)
  {
- y[i]=n%10;
- n/=10;
+ y[i]=Integer.parseInt(k.charAt(i)+"");
  }
- for(int i=1;i<=y.length;i++)
+ for(int i=1;i<=k.length();i++)
  {
   for(int j=0;j<i;j++)
-  {
   sum+=y[j];
-  }
 }
 }
-System.out.print(sum);
+System.out.println(sum);
 }
 }
