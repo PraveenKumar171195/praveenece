e997e00a924205fefc583d034fedfc39fabbd1f7
PraveenKumar171195 <praveenkumarece20@gmail.com>
2016-09-02 18:39:08 +0530

    Create spow.java

---
+++ spow.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class spow
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt(),c=0,pow=0;
+String p=n+"":
+while(n!=0)
+{
+c++;
+n/=10;
+}
+for(int i=0;i<c;i++)
+{
+ pow+=Math.pow(Integer.parseInt(k.charAt(i)+""),c);
+}
+System.out.println(pow);
+}
+}
